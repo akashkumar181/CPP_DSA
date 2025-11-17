1c1acc0e584a3a21ac97f72e63053adaaa0f594a
Akash Kumar <akash805198@fmail.com>
2025-11-17 22:22:13 +0530

    Variable Datatype Lecture 1

---
+++ variable_Datatype.cpp
@@ -0,0 +1,52 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+   cout<<"Hello Word"<<endl;
+//   //variable :-it is a conatinar which is store to value
+//   int age=25;
+//   cout<<age<<endl;
+//   cout<<"size: "<<sizeof(age)<<endl;
+//   char a='A';
+//   cout<<a<<endl;
+//   float b=3.5;
+//   bool flag=true;
+//   double e=78.124;
+//   cout<<e<<endl;
+//   //Typeconversion:-Converting a datatype to another
+//   //implicit
+//   //Small datatype store in big
+//   char grade='A';
+//   int val=grade;
+//   //right side ki value left side me store ho jati h mtlb grade ka value val me ho jayega
+//   cout<<val<<endl;
+//   //Explicit:-Doing by frocing--typecasting
+//   //large datatype store in small
+//   double price=123.45;
+//   int p=price;
+//   cout<<p<<endl;
+//   //Taking Input
+//   int t;
+//   cout<<"Enter your value: "<<endl;
+//   cin>>t;
+//   cout<<"Value: "<<t<<endl;
+//  //operator 
+//  //Q. sum of two number
+//  int x;
+//  int y;
+//  cout<<"Enter your value: "<<endl;
+//  cin>>x;
+//  cin>>y;
+//  cout<<"Sum :"<<x+y;
+ //Import concept
+ int a=10;
+ int b=a++;//phle store then upadte mtlb b ka value 10 hoga
+ cout<<"value of b:"<<b<<endl;
+ cout<<"value of a:"<<a<<endl;//a=11
+ int c=10;
+ int d=++c;//phle update hoge then d ka value 11 hoga
+ cout<<"Value of d: "<<d<<endl; //ans 11
+ int e=--d;
+  cout<<"Value of e: "<<e<<endl; //ans 10
+ return 0;
+}
